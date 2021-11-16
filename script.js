122059ce061de46958bf99be57a31e22d46fd01f
Your Name <you@example.com>
2021-11-16 23:01:56 +0600

    Get lyrics by clicking button

--- script.js
+++ script.js
@@ -3,8 +3,9 @@ const button=document.querySelector('.search-bar');
 button.onclick=loadTitle;
 artistName.oninput=loadTitle;
 const getLyricsContainer=document.querySelector('.search-result');
+const singleLyric=document.getElementById('single-lyric');
+const singleAlbum=document.getElementById('single-album');
 function loadTitle(){
-    console.log(artistName.value.length);
     if(artistName.value.length>0){
         fetch(`https://api.lyrics.ovh/suggest/${artistName.value}`)
         .then(res=>res.json())
@@ -15,6 +16,8 @@ function loadTitle(){
     }
     else{
         getLyricsContainer.innerHTML='';
+        singleLyric.innerText='';
+        singleAlbum.innerText='';
     }
 }
 function displayTitle(data){
@@ -22,15 +25,30 @@ function displayTitle(data){
     data.map((x,index)=>{
         if(index<10){
             getLyricsContainer.innerHTML+=`
-            <div class="single-result row align-items-center my-3 p-3">
+            <div class="single-result row align-items-center my-3 p-3" id='lyrics-${x.id}'>
                 <div class="col-md-9">
                     <h3 class="lyrics-name">${x.title}</h3>
-                    <p class="author lead">Album by <span>${x.artist.name}</span></p>
+                    <p class="author-lead">Album by <span>${x.artist.name}</span></p>
                 </div>
                 <div class="col-md-3 text-md-right text-center">
-                    <button id="${x.id}" class="get-lyrics btn btn-success">Get Lyrics</button>
+                    <button class="get-lyrics btn btn-success" onclick='getLyricsHandler(event)'>Get Lyrics</button>
                 </div>
             </div>`;
         }
     });
+}
+function getLyricsHandler(event){
+    const element=event.target;
+    const parentId="#"+element.parentNode.parentNode.id;
+    const lyricsName=document.querySelector(parentId+" div h3").innerText;
+    const leadAuthor=document.querySelector(parentId+" div p span").innerText;
+    fetch(`https://api.lyrics.ovh/v1/${leadAuthor}/${lyricsName}`)
+    .then(res=>res.json())
+    .then(data=>{
+        console.log(data.lyrics);
+        element.setAttribute('href','#single-lyric');
+        singleAlbum.innerText=lyricsName+' - '+leadAuthor;
+        singleLyric.innerText=data.lyrics;
+    })
+    .catch(error=>console.log(error));
 }
